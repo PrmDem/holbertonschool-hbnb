859e5dd4baa23da95407a71b559e3b48b3295525
Priam <priamdmy@gmail.com>
2025-10-14 10:14:25 +0200

    [fix] regex on one line otherwise not functional

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,8 +1,7 @@
 from app.models.base_model import BaseModel
 import re
 
-regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9] \
-                   +@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 # correct format to verify user email against
 
 
