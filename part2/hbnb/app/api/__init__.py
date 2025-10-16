f282350f213de20671d8298350460c285befd560
Priam <priamdmy@gmail.com>
2025-10-16 15:24:28 +0200

    [update] added places namespace

--- part2/hbnb/app/api/__init__.py
+++ part2/hbnb/app/api/__init__.py
@@ -1,1 +1,5 @@
+from flask_restx import Api
+from app.api.v1.places import api as places_ns
 
+api = Api(version='1.0', title='HBnB API', description='HBnB Application API')
+api.add_namespace(places_ns, path='/api/v1/places')
