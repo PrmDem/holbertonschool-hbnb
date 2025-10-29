454b259fb111c94dc3f38142dd1c3328b2dc99a6
Saniez-l <saniezl.christophe@gmail.com>
2025-10-29 13:56:17 +0100

    changing for db repository

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -6,11 +6,15 @@ from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 import config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app(config_class=config.DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
     bcrypt.init_app(app)
+    db.init_app(app)
 
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
