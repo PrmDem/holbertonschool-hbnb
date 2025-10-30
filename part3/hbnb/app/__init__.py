f079483f5410caba8f15f66972967e4378147cc9
Priam <priamdmy@gmail.com>
2025-10-30 11:54:28 +0100

    [fix] moved sqlalchemy import to extensions (circular import)

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from flask_restx import Api
-from app.extensions import bcrypt
+from app.extensions import bcrypt, db
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 import config
-from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
 
 def create_app(config_class=config.DevelopmentConfig):
     app = Flask(__name__)
