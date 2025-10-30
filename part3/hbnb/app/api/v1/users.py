835e3730ec658a7336202d447aa354f82909daa0
Priam <priamdmy@gmail.com>
2025-10-30 14:34:47 +0100

    [fix] update_user now uses user_data directly

--- part3/hbnb/app/api/v1/users.py
+++ part3/hbnb/app/api/v1/users.py
@@ -26,10 +26,10 @@ class UserList(Resource):
         if existing_user:
             return {'error': 'Email already registered'}, 400
 
-        #try:
-        new_user = facade.create_user(user_data)
-        #except:
-            #return {'error': 'invalid input data'}, 400
+        try:
+            new_user = facade.create_user(user_data)
+        except:
+            return {'error': 'invalid input data'}, 400
 
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
     
@@ -76,12 +76,6 @@ class UserResource(Resource):
             return {'error': 'User not found'}, 404
         
         user_data = api.payload
-        user_put.first_name = user_data.get('first_name', user_put.first_name)
-        user_put.last_name = user_data.get('last_name', user_put.last_name)
-        user_put.email = user_data.get('email', user_put.email)
-
-        #problem pour save update, ajout dans facade de update_user etde la ligne en dessous mais plantage....
-        # fonctionne sans sauvegarde juste return,voir avec priam gestion des classe user par exemple
-        #facade.update_user(user_id, user_put)
+        facade.update_user(user_id, user_data)
         
         return {'id': user_put.id, 'first_name': user_put.first_name, 'last_name': user_put.last_name, 'email': user_put.email}, 200
