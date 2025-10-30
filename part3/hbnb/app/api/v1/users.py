367e304a4564b5c939fa1e22c15a00c143086b3f
Priam <priamdmy@gmail.com>
2025-10-30 21:16:45 +0100

    [update] added DEL

--- part3/hbnb/app/api/v1/users.py
+++ part3/hbnb/app/api/v1/users.py
@@ -36,44 +36,10 @@ class UserCreate(Resource):
 
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
 
-@api.route('/admin')
-class AdminUserCreate(Resource):
-    """Class and endpoint used to create as Admin
-    As opposed to regular / from where users can be
-    created by non-admin;
-
-    This is to test our endpoints more easily
-    """
-    @api.expect(user_model, validate=True)
-    @api.response(201, 'User successfully created')
-    @api.response(400, 'Email already registered')
-    @api.response(400, 'Invalid input data')
-    @jwt_required()
-    def post(self):
-        current_user = get_jwt()
-        if not current_user.get('is_admin'):
-           return {'error': 'Admin privileges required'}, 403
-
-        user_data = api.payload
-        email = user_data.get('email')
-
-        # Check if email is already in use
-        if facade.get_user_by_email(email):
-            return {'error': 'Email already registered'}, 400
-
-        try:
-            new_user = facade.create_user(user_data)
-        except:
-            return {'error': 'User creation failed'}, 400
-
-        return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
-    
     @api.response(200, 'list of user')
     @api.response(500, 'Internal server error')
     def get(self):
-        """
-        retrieves all users
-        """
+        """retrieves all users"""
         try:
             user_all = facade.all_users()
             user_list = []
@@ -117,8 +83,7 @@ class AdminUserCreate(Resource):
         try:
             new_user = facade.create_user(user_data)
         except:
-            return {'error': 'invalid input data'}, 400
-
+            return {'error': 'User creation failed'}, 400
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
 
 
@@ -153,8 +118,23 @@ class AdminUserResource(Resource):
 
         try:
             facade.update_user(user_id, user_data)
-
         except:
             return {'error': 'invalid input data'}, 400
-        
         return {'id': user_put.id, 'first_name': user_put.first_name, 'last_name': user_put.last_name, 'email': user_put.email}, 200
+
+    @api.response(200, 'User deleted successfully')
+    @api.response(404, 'User not found')
+    @jwt_required()
+    def delete(self, user_id):
+        # retrieve user's id & permission level
+        current_user = get_jwt()
+        
+        # If 'is_admin' is part of the identity payload
+        if not current_user.get('is_admin') or current_user.get('id') != user_id:
+            return {'error': 'Unauthorised action'}, 403
+
+        try:
+            facade.delete_user(user_id)
+        except:
+            return {'error': 'User not found'}, 404
+        return {'message': 'User deleted successfully'}, 200
--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -20,72 +20,98 @@ class HBnBFacade:
         self.user_repo.add(user)
         return user
 
+    # Retrieves a specific user from their id
     def get_user(self, user_id):
         return self.user_repo.get(user_id)
 
+    # Retrieves a specific user from their email
     def get_user_by_email(self, email):
         return self.user_repo.get_user_by_email(email)
 
+    # Retrieves all users
     def all_users(self):
-        """return all users"""
         return self.user_repo.get_all()
 
+    # Updates some or all of a user's information
     def update_user(self, user_id, user_obj):
-        """update user with put"""
         return self.user_repo.update(user_id, user_obj)
 
+    # Deletes a user
+    def delete_user(self, user_id):
+        return self.user_repo.delete(user_id)
+
+    # Creates a new amenity, adds to repository
     def create_amenity(self, amenity_data):
         amenity = Amenity(**amenity_data)
         self.amenity_repo.add(amenity)
         return amenity
 
+    # Retrieves one amenity using its id
     def get_amenity(self, amenity_id):
         return self.amenity_repo.get(amenity_id)
 
+    # Retrieves all amenities
     def get_all_amenities(self):
         return self.amenity_repo.get_all()
 
+    # Updates an amenity
     def update_amenity(self, amenity_id, amenity_data):
         return self.amenity_repo.update(amenity_id, amenity_data)
 
+    # Deletes an amenity
+    def delete_amenity(self, amenity_id):
+        return self.amenity_repo.delete(amenity_id)
+
+    # Creates a new place, adds it to repo
     def create_place(self, place_data):
         place = Place(**place_data)
         self.place_repo.add(place)
         return place
 
+    # Retrieves a place using its id
     def get_place(self, place_id):
         return self.place_repo.get(place_id)
 
+    # Retrieves all places
     def get_all_places(self):
         return self.place_repo.get_all()
 
+    # Updates one or all of a place's info
     def update_place(self, place_id, place_data):
         return self.place_repo.update(place_id, place_data)
 
+    # Deletes a place
+    def delete_place(self, place_id):
+        return self.place_repo.delete(place_id)
+
+    # Creates a new review, adds it to repo
     def create_review(self, review_data):
         review = Review(**review_data)
         self.review_repo.add(review)
         return review
 
+    # Retrieves a review by its id
     def get_review(self, review_id):
         return self.review_repo.get(review_id)
 
+    # Retrieves all reviews
     def get_all_reviews(self):
         return self.review_repo.get_all()
 
+    # Retrieves all reviews written about a specific place
     def get_reviews_by_place(self, place_id):
         return self.review_repo.get_reviews_by_place(place_id)
 
+    # Updates all or part of a review's info
     def update_review(self, review_id, review_data):
         return self.review_repo.update(review_id, review_data)
 
+    # Deletes a review
     def delete_review(self, review_id):
         return self.review_repo.delete(review_id)
-    
+
+    # Checks whether a given user has already reviewed a given place
     def compare_review(self, user_id, place_id):
-        """
-        Verify if is not place of user or if it s the first review
-        """
         reviews = self.review_repo.get_all()
         for review in reviews:
             if review.user_id == user_id and review.place_id == place_id:
