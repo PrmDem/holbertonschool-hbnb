5cb2753db9c6634b5b8046f0cfda45230f357ad3
Saniez-l <saniezl.christophe@gmail.com>
2025-11-01 19:20:12 +0100

    fix name sql

--- part3/hbnb/app/sql/create.sql
+++ part3/hbnb/app/sql/create.sql
@@ -1,48 +1,48 @@
-CREATE TABLE User (
+CREATE TABLE users (
 
-    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    id CHAR(36) PRIMARY KEY,
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     email VARCHAR(255) UNIQUE,
     password VARCHAR(255),
-    is_admin BOOLEAN DEFAULT FALSE
+    is_admin INTEGER DEFAULT 0
 );
 
-CREATE TABLE Place (
+CREATE TABLE places (
 
-    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    id CHAR(36) PRIMARY KEY,
     title VARCHAR(255),
     description TEXT,
     price DECIMAL(10, 2),
     latitude FLOAT,
     longitude FLOAT,
     owner_id CHAR(36),
-    FOREIGN KEY (owner_id) REFERENCES User(id) ON DELETE CASCADE
+    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE Review (
+CREATE TABLE reviews (
 
-    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    id CHAR(36) PRIMARY KEY,
     text TEXT,
     rating INT CHECK (rating >= 1 AND rating <= 5),
     user_id CHAR(36),
     place_id CHAR(36),
-    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE,
-    FOREIGN KEY (place_id) REFERENCES Place(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (place_id) REFERENCES places(id) ON DELETE CASCADE,
     UNIQUE (user_id, place_id)
     );
 
-CREATE TABLE Amenity (
+CREATE TABLE amenities (
 
-    id CHAR(36) PRIMARY KEY DEFAULT (UUID()),
+    id CHAR(36) PRIMARY KEY,
     name VARCHAR(255) UNIQUE
 );
 
-CREATE TABLE Place_Amenity (
+CREATE TABLE place_amenity (
 
     place_id CHAR(36),
     amenity_id CHAR(36),
     PRIMARY KEY (place_id, amenity_id),
-    FOREIGN KEY (place_id) REFERENCES Place(id) ON DELETE CASCADE,
-    FOREIGN KEY (amenity_id) REFERENCES Amenity(id) ON DELETE CASCADE
+    FOREIGN KEY (place_id) REFERENCES places(id) ON DELETE CASCADE,
+    FOREIGN KEY (amenity_id) REFERENCES amenities(id) ON DELETE CASCADE
 );
