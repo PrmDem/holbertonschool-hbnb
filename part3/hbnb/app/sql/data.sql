c8777f08a37142b5e1779398fc48f5df139ff3a3
Saniez-l <saniezl.christophe@gmail.com>
2025-11-01 19:12:56 +0100

    fix sql name in data.sql

--- part3/hbnb/app/sql/data.sql
+++ part3/hbnb/app/sql/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO User (
+INSERT INTO users (
     id, first_name, last_name, email, password, is_admin
 )
 VALUES (
@@ -7,11 +7,11 @@ VALUES (
     'HBnB',
     'admin@hbnb.io',
     '$2a$12$pPIznr1L5mygg2OnvyUKWeqU/xeUIswVVg4WxSEW618zqwQCjZVka',
-    TRUE
+    1
 );
 
 
-INSERT INTO Amenity (id, name) VALUES
+INSERT INTO amenities (id, name) VALUES
 ('1de40f87-4c01-47ce-a788-6562f06f32d4', 'WiFi'),
 ('4f2987a7-c3c5-4a2f-932d-dbc68defe119', 'Swimming Pool'),
 ('6a98acf9-a4e9-4673-b0a7-cfe1107a9b1b', 'Air Conditioning');
