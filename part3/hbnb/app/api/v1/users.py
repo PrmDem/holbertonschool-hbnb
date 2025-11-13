f7a57b86b79ea3d15084260b5e2acf869fec717a
Priam <priamdmy@gmail.com>
2025-11-13 13:26:51 +0100

    [fix] removed non-admin post method

--- part3/hbnb/app/api/v1/users.py
+++ part3/hbnb/app/api/v1/users.py
@@ -15,13 +15,20 @@ user_model = api.model('User', {
 })
 
 @api.route('/')
-class UserCreate(Resource):
+class AdminUserCreate(Resource):
+    """Class and endpoint used to create a user
+    Only administrators have authorisation
+    """
     @api.expect(user_model, validate=True)
     @api.response(201, 'User successfully created')
     @api.response(400, 'Email already registered')
     @api.response(400, 'Invalid input data')
+    @jwt_required()
     def post(self):
-        """Register a new user"""
+        current_user = get_jwt()
+        if not current_user.get('is_admin'):
+           return {'error': 'Admin privileges required'}, 403
+
         user_data = api.payload
         email = user_data.get('email')
 
@@ -56,37 +63,6 @@ class UserCreate(Resource):
         except Exception as e:
             return {"error": f"error user list: {str(e)}"}, 500
 
-@api.route('/admin')
-class AdminUserCreate(Resource):
-    """Class and endpoint used to create as Admin
-    As opposed to regular / from where users can be
-    created by non-admin;
-
-    This is to test our endpoints more easily
-    """
-    @api.expect(user_model, validate=True)
-    @api.response(201, 'User successfully created')
-    @api.response(400, 'Email already registered')
-    @api.response(400, 'Invalid input data')
-    @jwt_required()
-    def post(self):
-        current_user = get_jwt()
-        if not current_user.get('is_admin'):
-           return {'error': 'Admin privileges required'}, 403
-
-        user_data = api.payload
-        email = user_data.get('email')
-
-        # Check if email is already in use
-        if facade.get_user_by_email(email):
-            return {'error': 'Email already registered'}, 400
-
-        try:
-            new_user = facade.create_user(user_data)
-        except Exception as e:
-            return {'error': str(e)}, 400
-
-        return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
 
 @api.route('/<user_id>')
 class AdminUserResource(Resource):
@@ -105,12 +81,12 @@ class AdminUserResource(Resource):
     @jwt_required()
     def put(self, user_id):
         """update user after verifying autorisations"""
-        current_user = get_jwt_identity()
+        current_user = get_jwt()
         
         # If 'is_admin' is part of the identity payload
         is_admin = current_user.get('is_admin', False)
         # If the user is modifying their own profile
-        is_owner = current_user.get('id') == user_id
+        is_owner = (get_jwt_identity() == user_id)
         
         # Refuses input from unauthorised users
         if not (is_admin or is_owner):
